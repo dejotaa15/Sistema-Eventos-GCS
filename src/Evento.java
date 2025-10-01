8d22a4887ae98a05b2af0d80f3070c626f75dd3c
dejotaa15 <joao.bianchi06@gmail.com>
2025-10-01 16:18:32 -0300

    primeiro develop

---
+++ src/Evento.java
@@ -0,0 +1,47 @@
+public class Evento {
+    private static int codigoBase = 100;
+    private int codigoEvento;
+    private String nomeEvento;
+    private String dataEvento;
+    private double valorIngresso;
+    private String nomeResponsavel;
+    private int totalIngressos;
+
+    public Evento(String nomeEvento, String dataEvento, double valorIngresso, String nomeResponsavel, int totalIngressos) {
+        this.codigoEvento = ++codigoBase;
+        this.nomeEvento = nomeEvento;
+        this.dataEvento = dataEvento;
+        this.valorIngresso = valorIngresso;
+        this.nomeResponsavel = nomeResponsavel;
+        this.totalIngressos = totalIngressos;
+    }
+
+    public int getCodigoEvento() {
+        return codigoEvento;
+    }
+
+    public String getNomeEvento() {
+        return nomeEvento;
+    }
+
+    public String getDataEvento() {
+        return dataEvento;
+    }
+
+    public double getValorIngresso() {
+        return valorIngresso;
+    }
+
+    public String getNomeResponsavel() {
+        return nomeResponsavel;
+    }
+
+    public int getTotalIngressos() {
+        return totalIngressos;
+    }
+
+    @Override
+    public String toString() {
+        return codigoEvento + " - " + nomeEvento + " | Data: " + dataEvento + " | R$" + valorIngresso + " | Resp: " + nomeResponsavel + " | Ingressos: " + totalIngressos;
+    }
+}
---
+++ src/Participante.java
@@ -0,0 +1,22 @@
+public class Participante {
+    private String nomeCompleto;
+    private String cpf;
+
+    public Participante(String nomeCompleto, String cpf) {
+        this.nomeCompleto = nomeCompleto;
+        this.cpf = cpf;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return nomeCompleto + " (CPF: " + cpf + ")";
+    }
+}
